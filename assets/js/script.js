edb519e80b959bfa22294b538cd5fa6f8a9cf5b6
abraham2212 <ibrahimsak@code.edu.az>
2023-02-08 00:18:19 +0400

    js

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+
+"use strict"
+
+let num=20;
+if(num%2==0)(num%7==0);{
+
+    console.log("cutdur")
+}else{
+
+    console.log("tekdir")
+}
+
+let arr =[10,12,14];
+for(let i=0;i,arr.length;i++){
+    if((arr[i])&& arr[i]==="number"){
+
+        if(arr[i]%2==0){
+            console.log(arr[i])
+        }
+    }
+}
+
+function showNumbers(n,m){
+    for(let i =n;i<m;i++){
+        console.log(i)
+    }
+}
+
+function getNum(){
+    let nums=argumets;
+    for(let i =0;i<nums.length;i++){
+        console.log(nums[i])
+    }
+}
+getNum(4,5,6)
+
+
+
+let name;
+for(let i=0;i<=20;i++){
+  console.log(i)
+}
+
+
+let n=20;
+for(let i=0;i<=n;i++){
+    if(i%2==1){
+        console.log(i)
+    }
+}
